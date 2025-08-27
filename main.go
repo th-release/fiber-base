4591ae011521ab53471304d142383a39c5986691
th-release <cth.release@gmail.com>
2025-08-27 09:28:54 +0900

    [update] 파일 구조 변경

--- main.go
+++ main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"log"
 
+	"cth.release/web"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	app := fiber.New()
-	SetupRoutes(app)
+	web.SetupRoutes(app)
 
 	err := app.Listen(":3000")
 	if err != nil {
